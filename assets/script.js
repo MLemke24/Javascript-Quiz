b5070a87586f374d1ab9ae017d1118c2055a9a75
Matt Lemke <mattrlemke@gmail.com>
2021-02-23 19:50:28 -0600

    i dont know whats wrong lol

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,4 @@
+var taskIdCounter = 0;
 var content = document.querySelector(".card")
 var question = document.querySelector(".banner")
 var start = document.querySelector(".start")
@@ -6,41 +7,51 @@ var question, answer, choices
 
 
 
-const quiz = [
+const questionOne = [
     {
         question: "Var stands for:",
         choices: ["Var", "Variable", "Variation", "Vertical"],
         answer: "Var"
-    },
+    }
+    ];
+
+const questionTwo = [
     {
         question: " Which of the following function of String object combines the text of two strings and returns a new string?",
         choices: ["add()", "merge()", "append()", "concat()"],
         answer: "concat()"
-    },
+    }
+    ];
+
+const questionThree = [
     {
         question: "The Function 'isNaN stands for:",
         choices: ["is Not a Name", "is Not a Number", "is Null and null", "None of the above"],
         answer: "is Not a Number"
-    },
+    }
+    ];
+
+const questionFour = [
     {
         question: "True or False: You MUST us an 'else' statement in order to use an 'if' statement",
         choices: ["True", "False"],
         answer: "False"
-    },
+    }
+    ];
+
+const questionFive = [
     {
         question: "Java is a nickname for Javascript:",
         choices: ["True", "False"],
         answer: "False"
-    },
-];
+    }
+    ];
+
 
 // Start quiz
-
-function startGame () {
-
-}
-
+function startGame(content) {
+    document.getElementById(".card").innerHTML = "";
+};
 
 
-
-start.addEventListener("click", startGame)
+ content.addEventListener("click", questionOne);
