f5f992938be577a6641d95cd8cf8e55d01989ecf
Matt Lemke <mattrlemke@gmail.com>
2021-02-26 13:06:16 -0600

    timer fixed and high score loaded

--- assets/script.js
+++ assets/script.js
@@ -4,11 +4,9 @@ var start = document.querySelector(".start")
 var elements = document.getElementsByClassName("answer")
 
 
-var question, answer, choices, ansanswerwered, time, question_number
+var question, answer, choices, answered, time, question_number
 
-
-
-
+// Questions Array
 let quiz = [
     {
         question: "Var stands for:",
@@ -26,16 +24,18 @@ let quiz = [
         answer: "is Not a Number"
     },
     {
-        question: "True or False: You MUST us an 'else' statement in order to use an 'if' statement",
-        choices: ["True", "False"],
-        answer: "False"
+        question: "Of these options, what is NOT a Javascript Type:",
+        choices: ["Number", "Boolean", "Undefined", "Array"],
+        answer: "Array"
     },
     {
-        question: "Java is a nickname for Javascript:",
-        choices: ["True", "False"],
-        answer: "False"
+        question: "Which of the following is NOT valid Pop up box:",
+        choices: ["Alert", "Confirm and", "Prompt", "All of the above"],
+        answer: "All of the above"
     },
 ];
+
+
 
 // Start Game 
 function startGame () {
@@ -50,6 +50,14 @@ function startGame () {
 
     load_question(question_number)
 
+    
+// For loop
+
+for (var i = 0; i < elements.length; i++) {
+
+    elements[i].addEventListener("click", answer_question);
+}
+
 }
 
 // Load Questions 
@@ -59,6 +67,9 @@ document.getElementById("option-01").innerHTML = quiz[question_number]["choices"
 document.getElementById("option-02").innerHTML = quiz[question_number]["choices"][1]
 document.getElementById("option-03").innerHTML = quiz[question_number]["choices"][2]
 document.getElementById("option-04").innerHTML = quiz[question_number]["choices"][3]
+
+
+
 console.log(question_number)
 }
 
@@ -85,34 +96,46 @@ document.getElementById("start").addEventListener("click", function(){
 function answer_question (event) {
 
 	answered = event.target.innerHTML
+    
+    if(answered == quiz[question_number]["answer"]) {
+       
+        time += 10 
 
-	if(answered == quiz[question_number]["answer"]) {
-        // console.log("answer")
-	 time++
-	// console.log(answered)
+	} else {
 
-	}
-
-	question_number += 1
-
-	load_question(question_number)
-
-    if (answered === answer) {
-        time += 10
-    } else {
         time -= 10
     }
-	
+
+    question_number += 1
+
+    if(question_number <= 4 ) {
+       
+        load_question(question_number)
+       } else {
+           document.getElementById("questions").style.display = "none"
+   
+           document.getElementById("time").style.display = "none"
+   
+           document.getElementById("log").style.display = "block"
+   
+           document.getElementById("highScore").innerHTML = time
+       }
+   
+
 }
 
+// load high scores page
 
 
 // High Scores Page
 
 start.addEventListener("click", startGame)
 
-
-
-for (var i = 0; i < elements.length; i++) {
-    elements[i].addEventListener("click", answer_question);
-}
+// fix question number count 
+// fix answer log 
+// fix timer 
+// load high scores page 
+//save high scores to local storage
+// make section for high scores to save 
+// attach high scores at top right to high scores saved
+// add restart game button 
